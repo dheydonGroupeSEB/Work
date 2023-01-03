dd2534ffc2afa58a621c252ff6adc63200509467
dheydonGroupeSEB <78132774+dheydonGroupeSEB@users.noreply.github.com>
2023-01-03 12:04:49 +1100

    Add files via upload

---
+++ Automation/disabledUser.ps1
@@ -0,0 +1,11 @@
+﻿Import-Module ActiveDirectory
+
+# Get all disabled users
+$disabledUsers = Get-ADUser -Filter {Enabled -eq $false -and GivenName -eq 'Jacob'} -SearchBase "OU=AU-SYD-Disabled Objects-Users,OU=AU-SYD-Disabled Objects,OU=AU-SYD,OU=AU, DC=as,DC=seb,DC=com"
+
+# Iterate through each disabled user
+foreach ($user in $disabledUsers) {
+  # Update the email auto-reply for the user
+  Set-MailboxAutoReplyConfiguration -Identity $user.SamAccountName -AutoReplyState Enabled -InternalMessage "We are sorry but this email account you have tried to use does not exist within our organisation." -ExternalMessage "We are sorry but this email account you have tried to use does not exist within our organisation."
+}
+
